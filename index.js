edb6416bf651036cba07bdcd6ec5c0f91ed4c693
triestpa <patrick.triest@gmail.com>
2017-12-14 18:43:30 +0545

    Bundle blockchain as module

---
+++ index.js
@@ -0,0 +1,13 @@
+const Block = require('./blockchain/block')
+const Wallet = require('./blockchain/wallet')
+const Transaction = require('./blockchain/transaction')
+const Blockchain = require('./blockchain/blockchain')
+const BlockChainNode = require('./blockchain/node')
+
+module.exports = {
+  Block,
+  Wallet,
+  Transaction,
+  Blockchain,
+  BlockChainNode
+}
